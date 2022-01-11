6d2f967734ca5a160eb6a10ee20bf52de17855bf
lim ho <dlagh123@gmail.com>
2022-01-11 13:30:30 +0900

    .

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function a() {
+    let arr = [1, 2, 3, 4, 5, 6];
+    console.log(arguments);
+    Array.prototype.forEach.call(arguments, function (el) { console.log(el); });
+}
+
+a(21, 1, 2, 3, 3, 5, 6, 6, 7, 8, 98, 6, 3)
